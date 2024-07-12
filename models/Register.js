b551e5a6acb34ee467153d6cc82aa88d572bcbad
krishnaveni44 <krishnaveni44@gmail.com>
2024-07-13 02:31:39 +0530

    1

--- models/Register.js
+++ models/Register.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose')
 
 const RegisterSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
+    // name: String,
+    // email: String,
+    // password: String
+    name: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+        minlength: 6,
+      },
 })
 
 const RegisterModel = mongoose.model("register", RegisterSchema);
